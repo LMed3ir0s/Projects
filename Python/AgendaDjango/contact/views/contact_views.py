ad39529d6f7e828bd466ab4671f1f1821354921e
LMed3ir0s <medeiros.data@gmail.com>
2026-03-31 14:57:11 -0300

    WIP: views/contact_views, single_contact

--- Python/AgendaDjango/contact/views/contact_views.py
+++ Python/AgendaDjango/contact/views/contact_views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from contact.models import Contact
+from django.http import Http404
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    print(contacts.query)
 
     context = {
         'contacts': contacts,
@@ -11,5 +14,25 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+
+    # if single_contact is None:
+    #     raise Http404()
+
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    print(single_contact.query)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
