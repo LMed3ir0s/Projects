09d8b52d360028211256bf8866a13ebbf771b7cf
LMed3ir0s <medeiros.data@gmail.com>
2026-04-03 19:25:42 -0300

    feat: implementa. partials/_pagination (paginção)

--- Python/AgendaDjango/contact/views/contact_views.py
+++ Python/AgendaDjango/contact/views/contact_views.py
@@ -1,15 +1,20 @@
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
+from django.core.paginator import Paginator
 # from django.http import Http404
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
-
     print(contacts.query)
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -33,12 +38,17 @@ def search(request):
         Q(email__icontains=search_value)
     )\
     .order_by('-id')
-
     print(contacts.query)
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
