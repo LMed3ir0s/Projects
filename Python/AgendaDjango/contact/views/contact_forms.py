c0eff3217478375df32606428ae6b56cfa42f1a6
LMed3ir0s <medeiros.data@gmail.com>
2026-04-04 14:16:18 -0300

    WIP: config. campos e widgets forms

--- Python/AgendaDjango/contact/views/contact_forms.py
+++ Python/AgendaDjango/contact/views/contact_forms.py
@@ -1,11 +1,14 @@
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
-from contact.models import Contact
-from django.core.paginator import Paginator
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 def create(request):
-    context = {
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
+    context = {
+        'form': ContactForm()
     }
 
     return render(
