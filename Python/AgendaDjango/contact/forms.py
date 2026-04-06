f1d83842517dc931b307382eb706a63ccd7a47e5
LMed3ir0s <medeiros.data@gmail.com>
2026-04-06 14:46:31 -0300

    WIP: implementa. validações forms

--- Python/AgendaDjango/contact/forms.py
+++ Python/AgendaDjango/contact/forms.py
@@ -8,7 +8,7 @@ class ContactForm(forms.ModelForm):
         widget=forms.TextInput(
             attrs={
                 'class': 'classe-a classe-b',
-                'placeholder': 'Aqui veio do init',
+                'placeholder': 'Digite um Nome',
             }
         ),
         label='Primeiro Nome',
@@ -26,7 +26,7 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
         fields = (
-            'first_name', 'last_name', 'phone',
+            'first_name', 'last_name', 'phone', 'email', 'description', 'category',
         )
         # widgets = {
         #     'first_name': forms.TextInput(
@@ -39,3 +39,32 @@ class ContactForm(forms.ModelForm):
 
     def clean(self):
         # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+        )
+            
+        self.add_error('first_name', msg)
+        self.add_error('last_name', msg)
+
+        return super().clean()
+    
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+
+        return first_name
