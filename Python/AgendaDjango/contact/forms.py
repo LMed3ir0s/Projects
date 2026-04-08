1d620c44d6f2f37c6283faaf6df888f920ba6323
LMed3ir0s <medeiros.data@gmail.com>
2026-04-08 11:16:06 -0300

    feat: implementa. picture

--- Python/AgendaDjango/contact/forms.py
+++ Python/AgendaDjango/contact/forms.py
@@ -4,41 +4,21 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class': 'classe-a classe-b',
-                'placeholder': 'Digite um Nome',
-            }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário',
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+        attrs={
+            'accept': 'image/*',
+        }
+        )
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a classe-b',
-        #     'placeholder': 'Aqui veio do init',
-        # })
 
     class Meta:
         model = models.Contact
         fields = (
-            'first_name', 'last_name', 'phone', 'email', 'description', 'category',
+            'first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture',
         )
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     )
-        # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
         cleaned_data = self.cleaned_data
         first_name = cleaned_data.get('first_name')
         last_name = cleaned_data.get('last_name')
