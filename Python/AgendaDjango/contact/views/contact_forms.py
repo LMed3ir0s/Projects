336c5abea3340d951196e8a7100fd3f32897ff41
LMed3ir0s <medeiros.data@gmail.com>
2026-04-10 11:51:51 -0300

    implementa: login required

--- Python/AgendaDjango/contact/views/contact_forms.py
+++ Python/AgendaDjango/contact/views/contact_forms.py
@@ -2,8 +2,10 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from contact.forms import ContactForm
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='contact:login ')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -36,7 +38,7 @@ def create(request):
         context
     )
 
-
+@login_required(login_url='contact:login ')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
@@ -72,7 +74,7 @@ def update(request, contact_id):
         context
     )
 
-
+@login_required(login_url='contact:login ')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
